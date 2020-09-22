f69dc125764ac6f1217b808dc8d167444d8340c0
Lightwave <thelonerd@gmail.com>
2020-09-22 11:48:10 +0300

    Implement index operator for LocalizedString.

--- TMRI.Primitives/LocalizedString.cs
+++ TMRI.Primitives/LocalizedString.cs
@@ -6,7 +6,22 @@ using TMRI.Primitives.Enums;
 namespace TMRI.Primitives
 {
     [JsonConverter(typeof(LocalizedStringConverter))]
-    public class LocalizedString: Dictionary<Language, string>
+    public class LocalizedString: Dictionary<Language, string>, IDictionary<Language, string>
     {
+        public new string this[Language key]
+        {
+            get => TryGetValue(key, out var value) ? value : base[Language.JP];
+            set
+            {
+                if (ContainsKey(key))
+                {
+                    base[key] = value;
+                }
+                else
+                {
+                    Add(key, value);
+                }
+            }
+        }
     }
 }
